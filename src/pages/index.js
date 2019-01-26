13b2e9511d2eaddc6147770edfb4f7fa072fd969
Emily Kauffman <kauffmanes@gmail.com>
2019-01-25 22:50:09 -0500

    yellow

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
       <h2 style={{ fontSize: '4rem'}}>[in•<strong>trep</strong>•id-<strong>em</strong>]</h2>
       <p><em>adjective</em> resolutely fearless, adventurous, undaunted, bold</p>
-      <p>The name of my portfolio is a combination of the word "intrepid", and my name, "Em". I fully believe that in order to be a leader in design and user experience, you must be audacious. When balanced with conventions and best practices, you will produce an innovative product.</p>
+      <p>Software engineer living in Pittsburgh</p>
       {/* <Link to="/page-2/">Go to page 2</Link> */}
     </div>
   </Layout>
